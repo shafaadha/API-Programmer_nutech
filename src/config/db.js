4a130c9e2057bbe15ce69b4d84008172b5fd316c
shafaadha <adhashafa@gmail.com>
2025-10-30 02:43:01 +0700

    fix database config

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,13 @@
 import mysql from 'mysql2/promise';
-import "../config/env.js"
 
 const db = await mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306
 });
+
+console.log('✅ Connected to MySQL successfully');
 
 export default db;
