d39cf5d33433ba23f9535136de4ffb8f74154f76
shafaadha <adhashafa@gmail.com>
2025-11-09 21:44:51 +0700

    fix(auth): handle database query

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,11 @@
 import db from "../config/db.js";
-import "../config/env.js"
+import "../config/env.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { successResponse, errorResponse } from "../utils/responseFormatter.js";
 
 const JWT_SECRET = process.env.JWT_SECRET;
+
 export const register = async (req, res) => {
   const { email, first_name, last_name, password } = req.body;
 
@@ -33,42 +34,47 @@ export const register = async (req, res) => {
       .json(errorResponse(102, "Password length minimal 8 karakter"));
   }
 
+  const connection = await db.getConnection();
+
   try {
-    const [existingUser] = await db.execute(
+    const [existingUser] = await connection.execute(
       "SELECT id FROM users WHERE email = ?",
       [email]
     );
 
     if (existingUser.length > 0) {
+      connection.release();
       return res.status(400).json(errorResponse(102, "Email sudah terdaftar"));
     }
 
-    await db.beginTransaction();
+    await connection.beginTransaction();
 
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
-    const profileImage = 'default-profile.png';
+    const profileImage = "default-profile.png";
 
-    //inser new user
-    const [newUser] = await db.execute(
+    const [newUser] = await connection.execute(
       "INSERT INTO users (email, first_name, last_name, password, profile_image) VALUES (?, ?, ?, ?, ?)",
       [email, first_name, last_name, hashedPassword, profileImage]
     );
 
     const userId = newUser.insertId;
-    //create wallet for new user
-    await db.execute(
+
+    await connection.execute(
       "INSERT INTO wallets (user_id, balance) VALUES (?, ?)",
       [userId, 0]
     );
 
-    await db.commit();
-
-    res.json(successResponse("Registrasi berhasil silahkan login"));
+    await connection.commit();
+    res.json(successResponse("Registrasi berhasil silakan login"));
   } catch (err) {
+    await connection.rollback();
     res.status(500).json(errorResponse(500, err.message));
+  } finally {
+    connection.release();
   }
 };
+
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
@@ -95,7 +101,7 @@ export const login = async (req, res) => {
   }
 
   try {
-    const [user] = await db.execute(
+    const [user] = await connection.execute(
       "SELECT*FROM users WHERE email=?",
       [email]
     );
